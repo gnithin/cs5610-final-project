644f03c22a37282cf12fe62ab4fb2d14aead26c5
Nithin <nithin.linkin@gmail.com>
2020-03-16 16:09:28 -0400

    [build] Add the basic route structure of the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AppRouter from "./appRouter";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <AppRouter/>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
