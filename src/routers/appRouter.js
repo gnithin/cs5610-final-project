bdc8b20548fd95c0ea1246d0aff9a2ff70321797
Nithin <nithin.linkin@gmail.com>
2020-03-24 15:01:45 -0400

    [answer] Fix formatting

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -5,35 +5,36 @@ import viewQuestion from "../components/displayQuestionAnswer";
 import Home from "../components/home";
 
 class AppRouter extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <Switch>
-            <Route
-                path="/create-question"
-                component={CreateQuestion}
-            />
+    
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/create-question"
+                        component={CreateQuestion}
+                    />
 
-            <Route
-                path="/home"
-                component={Home}
-            />
+                    <Route
+                        path="/home"
+                        component={Home}
+                    />
 
-            <Route
-                path="/view-question/:questionID"
-                component={viewQuestion}
-            />
+                    <Route
+                        path="/view-question/:questionID"
+                        component={viewQuestion}
+                    />
 
-            <Route
-                path="*"
-                render={() => {
-                  return (<Redirect to="/home"/>);
-                }}
-            />
-          </Switch>
-        </BrowserRouter>
-    );
-  }
+                    <Route
+                        path="*"
+                        render={() => {
+                            return (<Redirect to="/home"/>);
+                        }}
+                    />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default AppRouter;
