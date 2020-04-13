a3b58ee0012c5961cca230b95bc4ca21b878230f
Keshav Chandrashekar <chandrashekar.ke@husky.neu.edu>
2020-04-13 19:46:47 -0400

    fix2

--- src/components/navbar/NavBarComponent.js
+++ src/components/navbar/NavBarComponent.js
@@ -43,8 +43,6 @@ class NavBarComponent extends Component {
                         Logout
                     </button>
 
-
-
                 </div>
                 {/*<button className="navbar-toggler" type="button" data-toggle="collapse"*/}
                 {/*        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"*/}
