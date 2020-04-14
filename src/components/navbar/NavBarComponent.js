3d41feb740f3bf9f216e31654821678347d7f15b
viralbpatel <patel.viral@husky.neu.edu>
2020-04-14 15:25:36 -0400

    Removed commented code from Navbar component

--- src/components/navbar/NavBarComponent.js
+++ src/components/navbar/NavBarComponent.js
@@ -65,50 +65,7 @@ class NavBarComponent extends Component {
                             >
                                 Logout
                             </button>
-
-
                         </div>
-                        {/*<button className="navbar-toggler" type="button" data-toggle="collapse"*/}
-                        {/*        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"*/}
-                        {/*        aria-expanded="false" aria-label="Toggle navigation">*/}
-                        {/*    <span className="navbar-toggler-icon"></span>*/}
-                        {/*</button>*/}
-
-                        {/*<div className="collapse navbar-collapse nav-right " id="navbarSupportedContent">*/}
-                        {/*    <ul className="navbar-nav mr-auto ">*/}
-                        {/*        <li className="nav-item active">*/}
-                        {/*            <a className="nav-link" href="#">Home <span*/}
-                        {/*                className="sr-only">(current)</span></a>*/}
-                        {/*        </li>*/}
-                        {/*        <li className="nav-item">*/}
-                        {/*            <a className="nav-link" href="#">Link</a>*/}
-                        {/*        </li>*/}
-                        {/*        <li className="nav-item dropdown">*/}
-                        {/*            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"*/}
-                        {/*               role="button" data-toggle="dropdown" aria-haspopup="true"*/}
-                        {/*               aria-expanded="false">*/}
-                        {/*                Dropdown*/}
-                        {/*            </a>*/}
-                        {/*            <div className="dropdown-menu" aria-labelledby="navbarDropdown">*/}
-                        {/*                <a className="dropdown-item" href="#">Action</a>*/}
-                        {/*                <a className="dropdown-item" href="#">Another action</a>*/}
-                        {/*                <div className="dropdown-divider"></div>*/}
-                        {/*                <a className="dropdown-item" href="#">Something else here</a>*/}
-                        {/*            </div>*/}
-                        {/*        </li>*/}
-                        {/*        <li className="nav-item">*/}
-                        {/*            <a className="nav-link disabled" href="#" tabIndex="-1"*/}
-                        {/*               aria-disabled="true">Disabled</a>*/}
-                        {/*        </li>*/}
-                        {/*    </ul>*/}
-                        {/*    <div className="form-inline my-2 my-lg-0">*/}
-                        {/*        <input className="form-control mr-sm-2" type="search" placeholder="Search"*/}
-                        {/*               aria-label="Search"/>*/}
-                        {/*        <button className="btn btn-outline-success my-2 my-sm-0"*/}
-                        {/*                type="submit">Search*/}
-                        {/*        </button>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
                     </nav>
                 }
             </span>
