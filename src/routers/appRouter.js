1aa2688ef3e8e5b36ba30fdce1cfd14c47f52647
Nithin <nithin.linkin@gmail.com>
2020-04-18 17:18:00 -0400

    [home] Add redirect for the home page

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -53,6 +53,14 @@ class AppRouter extends Component {
                     />
 
                     <Route
+                        exact
+                        path="/"
+                        render={() => {
+                            return (<Redirect to="/welcome"/>);
+                        }}
+                    />
+
+                    <Route
                         path="*"
                         render={() => {
                             return (<Redirect to="/home"/>);
