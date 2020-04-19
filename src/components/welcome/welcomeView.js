534df1ad1dfb9a5f1b1347e6b0516d2d25fb0278
Nithin <nithin.linkin@gmail.com>
2020-04-19 16:57:53 -0400

    [welcome] Personalize the welcome page

--- src/components/welcome/welcomeView.js
+++ src/components/welcome/welcomeView.js
@@ -1,34 +1,67 @@
 import React from 'react';
 import './welcome.css'
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+import Utils from "../../common/utils";
 
-const WelcomeView = () => {
-    return (
-        <div className="wc-wrapper">
-            <div className="wc-contents">
-                <div>
-                    <h1 className="wc-header">Chowk</h1>
-                    <span className="wc-tagline">Where questions come to die!</span>
-                </div>
-                <div className="wc-links-wrapper">
-                    <Link to={"/login"}>
-                        <button className="btn btn-primary wc-link">Login</button>
-                    </Link>
-                    <Link to={"/register"}>
-                        <button className="btn btn-primary wc-link">Register</button>
-                    </Link>
-                </div>
-                <div className="wc-privacy-links-wrapper">
-                    <Link
-                        target="_blank"
-                        to={"/privacy-policy"}
-                    >
-                        Privacy Policy
-                    </Link>
+class WelcomeView extends React.Component {
+    render() {
+        return (
+            <div className="wc-wrapper">
+                <div className="wc-contents">
+                    <div>
+                        <h1 className="wc-header">Chowk</h1>
+                        <span className="wc-tagline">Where questions come to die!</span>
+                    </div>
+                    {this.renderUserLinks()}
+                    <div className="wc-privacy-links-wrapper">
+                        <Link
+                            target="_blank"
+                            to={"/privacy-policy"}
+                        >
+                            Privacy Policy
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    renderUserLinks() {
+        if (false === Utils.isNull(this.props.isLoggedIn) && true === this.props.isLoggedIn) {
+            let name = this.props.userDetails.name;
+            if (Utils.isNull(name)) {
+                name = "";
+            }
+
+            return (
+                <div className="wc-links-wrapper">
+                    <span className="wc-user-message">Hi {name}!</span>
+                    <Link to={"/home"}>
+                        <button className="btn btn-primary wc-link">Return Home</button>
+                    </Link>
+                </div>
+            );
+        }
+
+        return (
+            <div className="wc-links-wrapper">
+                <Link to={"/login"}>
+                    <button className="btn btn-primary wc-link">Login</button>
+                </Link>
+                <Link to={"/register"}>
+                    <button className="btn btn-primary wc-link">Register</button>
+                </Link>
+            </div>
+        );
+    }
+}
+
+const reduxToComponentMapper = (state) => {
+    return {
+        isLoggedIn: state.userProfile.isLoggedIn,
+        userDetails: state.userProfile.userDetails,
+    };
 };
 
-export default WelcomeView;
+export default connect(reduxToComponentMapper)(WelcomeView);
