30d83e510b82ba467edd80ac9542a429a2587690
Nithin <nithin.linkin@gmail.com>
2020-04-19 20:54:54 -0400

    [admin-users] Add the credentials include

--- src/services/adminUsersService.js
+++ src/services/adminUsersService.js
@@ -3,7 +3,10 @@ import CONSTANTS from "../common/constants";
 export default class AdminUsersService {
     static async fetchAllUsers() {
         let url = `${CONSTANTS.BASE_URL}/api/users/all`;
-        return fetch(url).then(resp => {
+        return fetch(
+            url,
+            {credentials: 'include'}
+        ).then(resp => {
             if (!resp.ok) {
                 throw new Error("couldn't fetch all users");
             }
