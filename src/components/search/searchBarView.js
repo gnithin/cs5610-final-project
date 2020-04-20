6b833cc69f76a13a4a3acbfbb58d784a83e35481
Nithin <nithin.linkin@gmail.com>
2020-04-19 22:29:12 -0400

    [search-questions] Add the logic to the search bar

--- src/components/search/searchBarView.js
+++ src/components/search/searchBarView.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
+import './searchBar.css'
+import Utils from "../../common/utils";
 
 class SearchBarView extends Component {
+    constructor(props) {
+        super(props);
+        let initialVal = props.initialVal;
+        if (Utils.isNull(initialVal)) {
+            initialVal = "";
+        }
+        this.state = {
+            query: initialVal
+        };
+    }
+
     render() {
         return (
-            <div>
-                Search bar
-
-            </div>
+            <form className="form-inline col-12 sb-wrapper" onSubmit={(e) => {
+                e.preventDefault();
+                this.props.queryCb(this.state.query);
+            }}>
+                <input
+                    className="form-control col-11"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={this.state.query}
+                    onChange={(e) => {
+                        this.setState({query: e.target.value});
+                    }}
+                    autoFocus={true}
+                />
+                <button className="btn btn-primary col-1" type="submit">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </button>
+            </form>
         );
     }
 }
--- src/components/search/searchContainer.js
+++ src/components/search/searchContainer.js
@@ -2,8 +2,29 @@ import React, {Component} from 'react';
 import SearchResultsView from "./searchResultsView";
 import SearchBarView from "./searchBarView";
 import NavBarComponent from "../navbar/NavBarComponent";
+import Utils from "../../common/utils";
+
+const SEARCH_PARAM = "query";
 
 class SearchContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        let query = new URLSearchParams(this.props.location.search);
+        this.initialQuery = query.get(SEARCH_PARAM);
+
+        this.state = {
+            isLoading: false,
+            isError: false,
+        };
+    }
+
+    componentDidMount() {
+        if (false === Utils.isEmptyStr(this.initialQuery)) {
+            this.performSearchHandler(this.initialQuery);
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -15,11 +36,27 @@ class SearchContainer extends Component {
 
     renderSearch() {
         return (
-            <React.Fragment>
-                <SearchBarView/>
-                <SearchResultsView/>
-            </React.Fragment>
+            <div className="container-fluid">
+                <div className="row">
+                    <SearchBarView
+                        initialVal={this.initialQuery}
+                        queryCb={this.performSearchHandler.bind(this)}
+                    />
+                </div>
+                <div className="row">
+                    <SearchResultsView
+                        isLoading={this.state.isLoading}
+                        isError={this.state.isError}
+                    />
+                </div>
+            </div>
         );
+    }
+
+    performSearchHandler(query) {
+        // TODO:
+        console.log("Got query! - ", query);
+        window.history.pushState({}, "", `?${SEARCH_PARAM}=${query}`)
     }
 }
 
