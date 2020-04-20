2d965e928b380d3776e2df71d35639483dfdbf6f
Nithin <nithin.linkin@gmail.com>
2020-04-19 21:53:50 -0400

    [search-questions] Point to the local server

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,6 @@
 const CONSTANTS = {
-    BASE_URL: `https://web-dev-project-server.herokuapp.com`
+    // BASE_URL: `https://web-dev-project-server.herokuapp.com`
+    BASE_URL: `http://localhost:2000`
 };
 
 export default CONSTANTS
