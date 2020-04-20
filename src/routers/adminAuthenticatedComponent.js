3273d87c5116a2c9ed2bc40e3bb7821e880341c5
Nithin <nithin.linkin@gmail.com>
2020-04-19 20:14:30 -0400

    [search] Add the basic admin-auth component

---
+++ src/routers/adminAuthenticatedComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import baseAuthComponent from "./baseAuthComponent";
+
+const adminAuthenticatedComponent = (WrappedComponent) => {
+    return baseAuthComponent(
+        WrappedComponent,
+        true,
+        false,
+        '/home',
+        true,
+    )
+};
+
+export default adminAuthenticatedComponent;
