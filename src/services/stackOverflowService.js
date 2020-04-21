c9d29646d9fe117e4d5ca4ef71ac2ea69d275994
Nithin <nithin.linkin@gmail.com>
2020-04-21 12:03:31 -0400

    [related-query] Use the free form api instead of the in-title api

--- src/services/stackOverflowService.js
+++ src/services/stackOverflowService.js
@@ -1,7 +1,7 @@
 const API_URL = "https://api.stackexchange.com/2.2";
 
 export const searchQuestions = (data) =>
-    fetch(API_URL + `/search?order=desc&sort=activity&intitle=${data}&site=stackoverflow`)
+    fetch(API_URL + `/search/advanced?order=desc&sort=activity&q=${data}&site=stackoverflow`)
         .then(res => {
             if (res.status === 200) {
                 return res.json()
